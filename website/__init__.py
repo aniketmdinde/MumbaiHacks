00112a5904e82a4f3feac7b5321c61509ec2b694
aniketmdinde <aniketmdinde@gmail.com>
2024-10-25 22:06:06 +0530

    Auth Completed

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+
+mongo = PyMongo()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'MumHacks'
+    app.config['MONGO_URI'] = 'mongodb+srv://aniketmdinde100:Aniket*99@mumhacks.vl8mb.mongodb.net/MumHacks?retryWrites=true&w=majority&appName=MumHacks'
+
+    mongo.init_app(app)
+    bcrypt.init_app(app)
 
     from .views import views
     from .auth import auth
